df3186ce39b085e0bd26f9746258440fd0d54877
namhk4311 <khanhnamgalaxy@gmail.com>
2023-02-19 09:15:31 +0700

    undone

---
+++ Function_inlab/3.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void encrypt(char* text, int shift) {
+    shift = shift % 26;
+    for (int i = 0; i < strlen(text); i++)
+    {
+        int ntext = (int)text[i] + shift;
+        if (ntext > (int)'Z' || ntext > (int)'z') ntext = ntext - 26;
+        text[i] = (char)ntext;
+    }
+}
+
+void decrypt(char* text, int shift) {
+     shift = shift % 26;
+    for (int i = 0; i < strlen(text); ++i)
+    {
+        text[i] = text[i] - shift;
+        if ('a' <= text[i] + shift && text[i] + shift <= 'z' && text[i] < 'a') text[i] = text[i] + ('z' - 'a' + 1);
+        else if ('A' <= text[i] + shift && text[i] + shift <= 'Z' && text[i] < 'A') text[i] = text[i] + ('Z' - 'A' + 1);
+    }   
+}
+
+int main()
+{
+    int n, shift; 
+    cin >> n >> shift;
+    char* text = new char[n+1];
+    for(int i = 0; i < n; i++) cin >> text[i];
+    text[n] = 0;
+
+    encrypt(text, shift);
+    cout << text << '\n';
+    decrypt(text, shift);
+    cout << text;
+
+    delete[] text;
+    return 0;
+}
